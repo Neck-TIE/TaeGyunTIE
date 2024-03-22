83dbffe722e9eef8eabb59582b12e603f4453385
taegyun1995 <needle12s@gmail.com>
2024-03-22 22:21:47 +0900

    A + B

---
+++ src/main/java/tie/solved/B5_1000.java
@@ -0,0 +1,26 @@
+package tie.solved;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class B5_1000 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int sum = sum(n, m);
+        bw.write(String.valueOf(sum));
+        bw.flush();
+        bw.close();
+    }
+
+    private static int sum(int n, int m) {
+        return n + m;
+    }
+
+}
