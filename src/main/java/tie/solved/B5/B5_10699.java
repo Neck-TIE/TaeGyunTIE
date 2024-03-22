41d65c0fea7910fb2c84b9b0702a209c6518f7f3
taegyun1995 <needle12s@gmail.com>
2024-03-22 22:30:00 +0900

    오늘 날짜

---
+++ src/main/java/tie/solved/B5/B5_10699.java
@@ -0,0 +1,22 @@
+package tie.solved.B5;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class B5_10699 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        LocalDate today = LocalDate.now();
+        String date = today.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+        bw.write(date);
+        bw.flush();
+        bw.close();
+    }
+
+}
