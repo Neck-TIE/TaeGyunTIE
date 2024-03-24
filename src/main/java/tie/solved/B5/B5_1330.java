08870c3a5a5a4422cc377e767e905a95525ae178
taegyun1995 <needle12s@gmail.com>
2024-03-24 23:51:01 +0900

    두 수 비교하기

---
+++ src/main/java/tie/solved/B5/B5_1330.java
@@ -0,0 +1,32 @@
+package tie.solved.B5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B5_1330 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        String result = compare(n, m);
+
+        System.out.println(result);
+    }
+
+    private static String compare(int n, int m) {
+        if (n > m) {
+            return ">";
+        } else if (n < m) {
+            return "<";
+        } else {
+            return "==";
+        }
+    }
+
+}
