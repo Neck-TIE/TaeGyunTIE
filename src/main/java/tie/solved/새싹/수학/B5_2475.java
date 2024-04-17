1812b54304c346f354c67c6555f021795a863b26
taegyun1995 <needle12s@gmail.com>
2024-04-18 00:15:02 +0900

    수학: 검증수

--- src/main/java/tie/solved/새싹/수학/B5_2475.java
+++ src/main/java/tie/solved/새싹/수학/B5_2475.java
@@ -9,7 +9,6 @@ public class B5_2475 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int sum = 0;
