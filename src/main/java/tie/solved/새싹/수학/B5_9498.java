14cb8f58ba45c1da6692987b1a4627bbb789e21c
taegyun1995 <needle12s@gmail.com>
2024-04-17 23:59:55 +0900

    새싹: 시험성적

---
+++ src/main/java/tie/solved/새싹/수학/B5_9498.java
@@ -0,0 +1,31 @@
+package tie.solved.새싹.수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B5_9498 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        String grade = getGrade(n);
+        System.out.println(grade);
+    }
+
+    private static String getGrade(int n) {
+        if (n >= 90 && n <= 100) {
+            return "A";
+        } else if (n >= 80 && n < 90) {
+            return "B";
+        } else if (n >= 70 && n < 80) {
+            return "C";
+        } else if (n >= 60 && n < 70) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
+}
