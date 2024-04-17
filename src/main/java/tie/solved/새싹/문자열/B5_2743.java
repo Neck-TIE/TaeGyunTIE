ea79a4399a0523887686d42e93453f01736d1351
taegyun1995 <needle12s@gmail.com>
2024-04-18 00:27:03 +0900

    문자열: 단어 길이 재기

---
+++ src/main/java/tie/solved/새싹/문자열/B5_2743.java
@@ -0,0 +1,18 @@
+package tie.solved.새싹.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B5_2743 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        int len = str.length();
+        
+        System.out.println(len);
+    }
+
+}
