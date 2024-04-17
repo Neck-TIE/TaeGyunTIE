250a8cf0d4956cb192644b45d7948bba2ae99031
taegyun1995 <needle12s@gmail.com>
2024-04-18 00:17:04 +0900

    출력: Hello World

--- src/main/java/tie/solved/새싹/출력/B5_2557.java
+++ src/main/java/tie/solved/새싹/출력/B5_2557.java
@@ -7,10 +7,7 @@ import java.io.OutputStreamWriter;
 public class B5_2557 {
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write("Hello World!");
-        bw.flush();
-        bw.close();
+        System.out.println("Hello World!");
     }
 
 }
