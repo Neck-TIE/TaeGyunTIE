c13932dd3b28de65d737f104e29dad149760e60d
taegyun1995 <needle12s@gmail.com>
2024-04-17 23:42:45 +0900

    수학: 긴자리 계산

---
+++ src/main/java/tie/solved/Bronze5/수학/B5_2338.java
@@ -0,0 +1,30 @@
+package tie.solved.Bronze5.수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class B5_2338 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BigInteger n1 = new BigInteger(br.readLine());
+        BigInteger n2 = new BigInteger(br.readLine());
+
+        BigInteger[] result = calculateValue(n1, n2);
+        Arrays.stream(result).forEach(System.out::println);
+    }
+
+    private static BigInteger[] calculateValue(BigInteger n1, BigInteger n2) {
+        BigInteger[] answer = new BigInteger[3];
+        answer[0] = n1.add(n2);
+        answer[1] = n1.subtract(n2);
+        answer[2] = n1.multiply(n2);
+
+        return answer;
+    }
+
+}
