8098970071e2a802057d1cae52ad217f0783b349
taegyun1995 <needle12s@gmail.com>
2024-04-17 23:56:18 +0900

    새싹: Hello World

---
+++ src/main/java/tie/solved/새싹/출력/B5_2557.java
@@ -0,0 +1,16 @@
+package tie.solved.새싹.출력;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class B5_2557 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("Hello World!");
+        bw.flush();
+        bw.close();
+    }
+
+}
