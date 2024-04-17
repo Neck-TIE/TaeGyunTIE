9edcacd009d750d253386645763cdd60c0ae078f
taegyun1995 <needle12s@gmail.com>
2024-04-18 00:13:37 +0900

    수학: A / B

--- src/main/java/tie/solved/새싹/수학/B5_1008.java
+++ src/main/java/tie/solved/새싹/수학/B5_1008.java
@@ -7,16 +7,13 @@ public class B5_1008 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
         double result = divide(n, m);
-        bw.write(String.valueOf(result));
-        bw.flush();
-        bw.close();
+        System.out.println(result);
     }
 
     private static double divide(int n, int m) {
