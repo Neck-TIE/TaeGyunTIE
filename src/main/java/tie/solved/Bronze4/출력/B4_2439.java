8d8fb03f741eb481e2160c5cb749e7f9b1df06f1
taegyun1995 <needle12s@gmail.com>
2024-04-18 00:21:43 +0900

    출력: 별 찍기 -2

---
+++ src/main/java/tie/solved/Bronze4/출력/B4_2439.java
@@ -0,0 +1,25 @@
+package tie.solved.Bronze4.출력;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B4_2439 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= n - i; ++j) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; ++j) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
