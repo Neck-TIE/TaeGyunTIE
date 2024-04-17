2145dddd463d6dcb8c7393030adce06accf308da
taegyun1995 <needle12s@gmail.com>
2024-04-18 00:21:13 +0900

    문자열: 모음의 개수

---
+++ src/main/java/tie/solved/Bronze4/문자열/B4_1264.java
@@ -0,0 +1,37 @@
+package tie.solved.Bronze4.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B4_1264 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String str = br.readLine().toLowerCase();
+
+            if (str.equals("#")) {
+                break;
+            }
+
+            int answer = getNumberOfVowels(str);
+
+            System.out.println(answer);
+        }
+    }
+
+    private static int getNumberOfVowels(String str) {
+        int answer = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+
+}
