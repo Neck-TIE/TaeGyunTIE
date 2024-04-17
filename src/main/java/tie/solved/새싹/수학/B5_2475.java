0fa2b275d6e2716c54c2426631cf05fb0114df43
taegyun1995 <needle12s@gmail.com>
2024-04-17 23:55:57 +0900

    새싹: 검증수

---
+++ src/main/java/tie/solved/새싹/수학/B5_2475.java
@@ -0,0 +1,26 @@
+package tie.solved.새싹.수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B5_2475 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            sum += num * num;
+        }
+
+        int result = sum % 10;
+        System.out.println(result);
+    }
+
+}
