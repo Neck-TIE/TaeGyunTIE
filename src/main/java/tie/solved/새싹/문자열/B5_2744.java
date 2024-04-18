fff58733246fc73f58b7837476e0e322aada8d89
gyunny <needle12s@gmail.com>
2024-04-18 12:35:49 +0900

    문자열: 대소문자 바꾸기

---
+++ src/main/java/tie/solved/새싹/문자열/B5_2744.java
@@ -0,0 +1,31 @@
+package tie.solved.새싹.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B5_2744 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] chars = br.readLine().toCharArray();
+
+        String result = caseConversion(chars);
+        System.out.println(result);
+    }
+
+    private static String caseConversion(char[] chars) {
+        StringBuilder sb = new StringBuilder();
+
+        for(char c : chars) {
+            if(Character.isUpperCase(c)) {
+                sb.append(Character.toLowerCase(c));
+            } else {
+                sb.append(Character.toUpperCase(c));
+            }
+        }
+
+        return sb.toString();
+    }
+
+}
