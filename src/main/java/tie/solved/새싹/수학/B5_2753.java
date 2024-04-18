f24855d8982da0ed1a84d06108796b85774c316a
taegyun1995 <needle12s@gmail.com>
2024-04-18 23:40:42 +0900

    수학: 윤년

---
+++ src/main/java/tie/solved/새싹/수학/B5_2753.java
@@ -0,0 +1,27 @@
+package tie.solved.새싹.수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B5_2753 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int result = getLeapYear(n);
+        System.out.println(result);
+    }
+
+    private static int getLeapYear(int n) {
+        if(n % 4 == 0) {
+            if (n % 100 != 0 || n % 400 == 0) {
+                return 1;
+            }
+            return 0;
+        }
+        return 0;
+    }
+
+}
