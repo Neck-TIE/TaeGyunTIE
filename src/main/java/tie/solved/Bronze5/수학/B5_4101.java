27caef34611885df47929fd70ac5a787fc2b527a
taegyun1995 <needle12s@gmail.com>
2024-04-19 23:35:59 +0900

    수학: 크냐?

--- src/main/java/tie/solved/Bronze5/수학/B5_4101.java
+++ src/main/java/tie/solved/Bronze5/수학/B5_4101.java
@@ -12,14 +12,15 @@ public class B5_4101 {
 
         while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int num1 = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
             int num2 = Integer.parseInt(st.nextToken());
 
-            if (num1 == 0 && num2 == 0) {
+            if (num == 0 && num2 == 0) {
                 break;
             }
 
-            System.out.println(num1 > num2 ? "Yes" : "No");
+            String result = num > num2 ? "Yes" : "No";
+            System.out.println(result);
         }
     }
 
