c3181da317f5d2dd5577761cdd9449c1006a940a
taegyun1995 <needle12s@gmail.com>
2024-04-21 21:41:19 +0900

    수학: 킹,퀸,룩,비숍,나이트,폰

---
+++ src/main/java/tie/solved/Bronze5/수학/B5_3003.java
@@ -0,0 +1,25 @@
+package tie.solved.Bronze5.수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B5_3003 {
+
+    public static final int[] CHESS = {1, 1, 2, 2, 2, 8};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] oldChess = new int[6];
+        for (int i = 0; i < 6; i++) {
+            oldChess[i] = CHESS[i] - Integer.parseInt(st.nextToken());
+        }
+
+        for(int c : oldChess) {
+            System.out.print(c + " ");
+        }
+    }
+}
