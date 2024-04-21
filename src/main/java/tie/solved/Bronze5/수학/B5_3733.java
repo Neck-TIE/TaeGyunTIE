72eabdb04cefdd116730f18c391781ede5e062c2
taegyun1995 <needle12s@gmail.com>
2024-04-21 21:49:22 +0900

    수학: Shares

---
+++ src/main/java/tie/solved/Bronze5/수학/B5_3733.java
@@ -0,0 +1,22 @@
+package tie.solved.Bronze5.수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B5_3733 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+
+            System.out.println(s / (n + 1));
+        }
+    }
+
+}
