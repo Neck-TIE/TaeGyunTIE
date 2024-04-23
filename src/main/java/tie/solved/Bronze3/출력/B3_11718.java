dc086d00e1f9286500afacda0b615dc4d8b3f8f3
taegyun1995 <needle12s@gmail.com>
2024-04-24 00:12:07 +0900

    출력: 그대로 출력하기

---
+++ src/main/java/tie/solved/Bronze3/출력/B3_11718.java
@@ -0,0 +1,18 @@
+package tie.solved.Bronze3.출력;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B3_11718 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str;
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+    }
+
+}
