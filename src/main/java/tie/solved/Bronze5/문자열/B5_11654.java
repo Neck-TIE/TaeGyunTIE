e805d469b0c886ba7634bc334d810805be8075d7
taegyun1995 <needle12s@gmail.com>
2024-04-24 23:15:56 +0900

    수학: 아스키 코드

---
+++ src/main/java/tie/solved/Bronze5/문자열/B5_11654.java
@@ -0,0 +1,16 @@
+package tie.solved.Bronze5.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B5_11654 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char n = br.readLine().charAt(0);
+
+        System.out.println((int) n);
+    }
+
+}
