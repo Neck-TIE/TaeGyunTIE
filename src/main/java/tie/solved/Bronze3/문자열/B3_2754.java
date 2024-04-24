e88efaf107c944431964cd67c204eaca67cde30d
taegyun1995 <needle12s@gmail.com>
2024-04-24 23:31:38 +0900

    문자열: 학점계산

---
+++ src/main/java/tie/solved/Bronze3/문자열/B3_2754.java
@@ -0,0 +1,38 @@
+package tie.solved.Bronze3.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B3_2754 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        double grade;
+        char c = s.charAt(0);
+
+        if (c == 'A') {
+            grade = 4;
+        } else if (c == 'B') {
+            grade = 3;
+        } else if (c == 'C') {
+            grade = 2;
+        } else if (c == 'D') {
+            grade = 1;
+        } else {
+            System.out.print("0.0");
+            return;
+        }
+
+        c = s.charAt(1);
+        if (c == '+') {
+            grade += 0.3;
+        } else if (c == '-') {
+            grade -= 0.3;
+        }
+
+        System.out.print(grade);
+    }
+
+}
