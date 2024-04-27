347a486fd3a324f41b8fc4d52775133285624c9c
taegyun1995 <needle12s@gmail.com>
2024-04-28 00:29:52 +0900

    문자열: 문자와 문자열

---
+++ src/main/java/tie/solved/Bronze5/문자열/B5_27866.java
@@ -0,0 +1,18 @@
+package tie.solved.Bronze5.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B5_27866 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+
+        char c = str.charAt(i - 1);
+        System.out.println(c);
+    }
+
+}
