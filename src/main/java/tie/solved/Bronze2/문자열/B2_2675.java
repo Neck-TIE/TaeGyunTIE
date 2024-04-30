538515bc571ace2e9ca4b26eadeb8735e5ce1928
taegyun1995 <needle12s@gmail.com>
2024-04-30 20:32:16 +0900

    문자열: 문자열 반복

---
+++ src/main/java/tie/solved/Bronze2/문자열/B2_2675.java
@@ -0,0 +1,33 @@
+package tie.solved.Bronze2.문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B2_2675 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            String s = st.nextToken();
+
+            for (int j = 0; j < s.length(); j++) {
+                int count = 0;
+
+                while (count < n) {
+                    sb.append(s.charAt(j));
+                    count++;
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
