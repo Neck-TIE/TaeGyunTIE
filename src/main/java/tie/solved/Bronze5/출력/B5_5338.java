da5edad95d076a34e1b08d84b3182462a70430a1
taegyun1995 <needle12s@gmail.com>
2024-05-01 23:30:12 +0900

    출력: 마이크로소프트 로고

---
+++ src/main/java/tie/solved/Bronze5/출력/B5_5338.java
@@ -0,0 +1,13 @@
+package tie.solved.Bronze5.출력;
+
+public class B5_5338 {
+
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.print("'-..-'|_.-''-._|");
+    }
+
+}
