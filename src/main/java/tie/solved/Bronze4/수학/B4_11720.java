fee33dbe720bb2f857ae47409f2a621b5a13d5b2
taegyun1995 <needle12s@gmail.com>
2024-05-02 22:42:52 +0900

    수학: 숫자의 합

---
+++ src/main/java/tie/solved/Bronze4/수학/B4_11720.java
@@ -0,0 +1,24 @@
+package tie.solved.Bronze4.수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B4_11720 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] str = br.readLine().split("");
+
+        int sum = 0;
+
+        for (String s : str) {
+            sum += Integer.parseInt(s);
+        }
+
+        System.out.println(sum);
+
+    }
+
+}
