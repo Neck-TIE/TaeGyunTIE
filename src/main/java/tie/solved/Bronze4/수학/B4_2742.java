d18f17271146ac86e32e24b6686d9d8e0e48f9cf
taegyun1995 <needle12s@gmail.com>
2024-05-03 20:18:16 +0900

    수학: 기찍 N

---
+++ src/main/java/tie/solved/Bronze4/수학/B4_2742.java
@@ -0,0 +1,19 @@
+package tie.solved.Bronze4.수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
+
+public class B4_2742 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        IntStream.rangeClosed(1, n)
+                .map(i -> n - i + 1)
+                .forEach(System.out::println);
+    }
+
+}
