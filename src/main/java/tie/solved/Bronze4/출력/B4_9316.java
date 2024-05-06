7af9c72b9d3cab38fad7f718951c35d21deae0f0
taegyun1995 <needle12s@gmail.com>
2024-05-06 13:28:15 +0900

    출력: Hello Judge

---
+++ src/main/java/tie/solved/Bronze4/출력/B4_9316.java
@@ -0,0 +1,18 @@
+package tie.solved.Bronze4.출력;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B4_9316 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Hello World, Judge " + i + "!");
+        }
+    }
+
+}
