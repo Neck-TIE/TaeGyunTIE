8b041831fdb2b38593be5ed24af479fa80ee415e
taegyun1995 <needle12s@gmail.com>
2024-05-11 21:08:35 +0900

    Java: 변수

---
+++ src/main/java/tie/inflearn/java_introduce/variable/Var1.java
@@ -0,0 +1,15 @@
+package tie.inflearn.java_introduce.variable;
+
+public class Var1 {
+
+    public static void main(String[] args) {
+/*        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);*/
+
+        System.out.println(20); // 변경 10 -> 20
+        System.out.println(20); // 변경 10 -> 20
+        System.out.println(20); // 변경 10 -> 20
+    }
+
+}
---
+++ src/main/java/tie/inflearn/java_introduce/variable/Var6.java
@@ -0,0 +1,10 @@
+package tie.inflearn.java_introduce.variable;
+
+public class Var6 {
+
+    public static void main(String[] args) {
+        int a;
+        // System.out.println(a);
+    }
+
+}
