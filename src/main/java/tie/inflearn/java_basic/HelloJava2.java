37e094c818634bfd343dc1ffdd816b7115ee6515
taegyun1995 <needle12s@gmail.com>
2024-05-11 19:14:00 +0900

    Hello Java System out println practice java program

---
+++ src/main/java/tie/inflearn/java_basic/HelloJava2.java
@@ -0,0 +1,12 @@
+package tie.inflearn.java_basic;
+
+public class HelloJava2 {
+
+    // 프로그램은 main()을 시작으로 위에서 아래로 한 줄씩 실행
+    public static void main(String[] args) {
+        System.out.println("Hello Java1");
+        System.out.println("Hello Java2");
+        System.out.println("Hello Java3");
+    }
+
+}
