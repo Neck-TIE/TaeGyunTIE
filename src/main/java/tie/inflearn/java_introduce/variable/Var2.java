f55552d528eaa28a7748e1e2fb38b79119057626
taegyun1995 <needle12s@gmail.com>
2024-05-11 21:08:35 +0900

    Java: 변수

---
+++ src/main/java/tie/inflearn/java_introduce/variable/Var2.java
@@ -0,0 +1,17 @@
+package tie.inflearn.java_introduce.variable;
+
+public class Var2 {
+
+    public static void main(String[] args) {
+        // 변수 값 대입
+        int a; // 변수 선언
+        // a = 10;
+        a = 20; // 변수 초기화, 10 -> 20
+
+        // 변수 값 읽기
+        System.out.println(a); // 20
+        System.out.println(a); // 20
+        System.out.println(a); // 20
+    }
+
+}
---
+++ src/main/java/tie/inflearn/java_introduce/variable/VarEx3Question.java
@@ -0,0 +1,13 @@
+package tie.inflearn.java_introduce.variable;
+
+public class VarEx3Question {
+
+    public static void main(String[] args) {
+        long longVar = 100000000000L;
+        System.out.println("longVar = " + longVar);
+
+        boolean booleanVar = true;
+        System.out.println("booleanVar = " + booleanVar);
+    }
+
+}
