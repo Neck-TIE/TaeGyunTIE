94d42c8b738bc1f41c912e136b21ba3108f44013
taegyun1995 <needle12s@gmail.com>
2024-05-11 19:14:00 +0900

    Hello Java System out println practice java program

---
+++ src/main/java/tie/inflearn/java_basic/HelloJava.java
@@ -0,0 +1,13 @@
+package tie.inflearn.java_basic;
+
+// HelloJava 클래스 파일의 범위 시작
+public class HelloJava {
+
+    // main 메서드, 프로그램의 시작점
+    public static void main(String[] args) {
+        // 값을 콘솔에 출력해주는 기능
+        // 자바는 문자열을 사용할 때 "" 쌍따옴표를 사용
+        System.out.println("Hello Java");
+    } // main 메서드의 범위 끝
+
+} // HelloJava 클래스의 범위 끝
