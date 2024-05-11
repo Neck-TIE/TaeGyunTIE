664346c2f1065be521193166f70498b3a8aedf6d
taegyun1995 <needle12s@gmail.com>
2024-05-11 19:16:41 +0900

    java comment study

---
+++ src/main/java/tie/inflearn/java_basic/주석/CommentJava.java
@@ -0,0 +1,15 @@
+package tie.inflearn.java_basic.주석;
+
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java1"); // Hello Java1을 출력합니다.
+        //System.out.println("Hello Java2"); // 한줄 주석
+
+        /* 여러 줄 주석, 이 사이의 모든 텍스트가 주석 처리
+        System.out.println("Hello Java3");
+        System.out.println("Hello Java4");
+        */
+    }
+
+}
