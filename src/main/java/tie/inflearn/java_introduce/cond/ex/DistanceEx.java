115f333e17e63e5fa3169a91f4a88b2aa3d03b84
taegyun1995 <needle12s@gmail.com>
2024-05-19 23:11:37 +0900

    Java: 연산자

---
+++ src/main/java/tie/inflearn/java_introduce/cond/ex/DistanceEx.java
@@ -0,0 +1,19 @@
+package tie.inflearn.java_introduce.cond.ex;
+
+public class DistanceEx {
+
+    public static void main(String[] args) {
+        int distance = 25;
+
+        if (distance <= 1) {
+            System.out.println("도보로 이동합니다.");
+        } else if (distance <= 10) {
+            System.out.println("자전거로 이동합니다.");
+        } else if (distance <= 100) {
+            System.out.println("자동차로 이동합니다.");
+        } else {
+            System.out.println("비행기로 이동합니다.");
+        }
+    }
+
+}
