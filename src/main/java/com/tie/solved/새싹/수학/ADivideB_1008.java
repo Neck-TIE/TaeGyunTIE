14341eedba821f5fabd24a49d7931335352775a2
gyunny <needle12s@gmail.com>
2024-08-07 11:15:31 +0900

    새싹 : A / B

--- src/main/java/com/tie/solved/새싹/수학/ADivideB_1008.java
+++ src/main/java/com/tie/solved/새싹/수학/ADivideB_1008.java
@@ -3,7 +3,7 @@ package com.tie.solved.새싹.수학;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class B5_1008 {
+public class ADivideB_1008 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
