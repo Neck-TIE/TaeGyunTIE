0b4bf5b8d34d69f91ddb0b0efe0cf13cb066c897
gyunny <needle12s@gmail.com>
2024-08-07 12:20:43 +0900

    Bronze Lv5 : Moo

---
+++ src/main/java/com/tie/solved/Bronze5/출력/Moo_1809.java
@@ -0,0 +1,14 @@
+package com.tie.solved.Bronze5.출력;
+
+public class Moo_1809 {
+
+    public static void main(String[] args) {
+        System.out.println("(___)");
+        System.out.println("(o o)____/");
+        System.out.println(" @@      \\");
+        System.out.println("  \\ ____,/");
+        System.out.println("  //   //");
+        System.out.print(" ^^   ^^");
+    }
+
+}
