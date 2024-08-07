85166f92f71f850be75f38762d7e84b9ef520cb0
gyunny <needle12s@gmail.com>
2024-08-07 12:41:46 +0900

    새싹 : 구구단

--- src/main/java/com/tie/solved/새싹/수학/구구단_2739.java
+++ src/main/java/com/tie/solved/새싹/수학/구구단_2739.java
@@ -1,19 +1,18 @@
-package com.tie.solved.Bronze5.수학;
+package com.tie.solved.새싹.수학;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class B5_2739 {
+public class 구구단_2739 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        for(int i = 1; i <= 9; i++) {
+        for (int i = 1; i <= 9; i++) {
             System.out.println(n + " * " + i + " = " + n * i);
         }
-
     }
 
 }
