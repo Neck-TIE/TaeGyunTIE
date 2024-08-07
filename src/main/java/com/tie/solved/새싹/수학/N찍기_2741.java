58932a61c1d1291368fe371f1c00ea01e19d3fd4
gyunny <needle12s@gmail.com>
2024-08-07 12:42:43 +0900

    새싹 : N 찍기

--- src/main/java/com/tie/solved/새싹/수학/N찍기_2741.java
+++ src/main/java/com/tie/solved/새싹/수학/N찍기_2741.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class B5_2741 {
+public class N찍기_2741 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
