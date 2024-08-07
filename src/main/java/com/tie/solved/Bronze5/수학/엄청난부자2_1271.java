fc735d34e91d15aa963fa3cb0e4b6dac222e3b49
gyunny <needle12s@gmail.com>
2024-08-07 11:34:51 +0900

    Bronze Lv5 : 엄청난 부자 2

---
+++ src/main/java/com/tie/solved/Bronze5/수학/엄청난부자2_1271.java
@@ -0,0 +1,30 @@
+package com.tie.solved.Bronze5.수학;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class 엄청난부자2_1271 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        BigInteger n = new BigInteger(input[0]);
+        BigInteger m = new BigInteger(input[1]);
+
+        BigInteger[] result = solution(n, m);
+        for (BigInteger i : result) {
+            System.out.println(i);
+        }
+    }
+
+    private static BigInteger[] solution(BigInteger n, BigInteger m) {
+        BigInteger[] answer = new BigInteger[2];
+        answer[0] = n.divide(m);
+        answer[1] = n.remainder(m);
+
+        return answer;
+    }
+
+}
