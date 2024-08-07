2e4fef105088f6e2b72de840a7f282f4e1527d37
gyunny <needle12s@gmail.com>
2024-08-07 12:24:21 +0900

    새싹 : 별 찍기 - 1

--- src/main/java/com/tie/solved/새싹/출력/별찍기_1_2438.java
+++ src/main/java/com/tie/solved/새싹/출력/별찍기_1_2438.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class B5_2438 {
+public class 별찍기_1_2438 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
