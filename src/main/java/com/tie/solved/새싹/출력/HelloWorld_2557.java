a0d9d3a2634cf29172ececdad1c77be15a793634
gyunny <needle12s@gmail.com>
2024-08-07 12:36:08 +0900

    새싹 : Hello World

--- src/main/java/com/tie/solved/새싹/출력/HelloWorld_2557.java
+++ src/main/java/com/tie/solved/새싹/출력/HelloWorld_2557.java
@@ -1,10 +1,8 @@
 package com.tie.solved.새싹.출력;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 
-public class B5_2557 {
+public class HelloWorld_2557 {
 
     public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
