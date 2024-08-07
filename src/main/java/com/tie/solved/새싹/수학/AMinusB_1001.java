f51420b12cad590ead8607413efaf02d9142760c
gyunny <needle12s@gmail.com>
2024-08-07 11:13:29 +0900

    새싹 : A - B

--- src/main/java/com/tie/solved/새싹/수학/AMinusB_1001.java
+++ src/main/java/com/tie/solved/새싹/수학/AMinusB_1001.java
@@ -3,7 +3,7 @@ package com.tie.solved.새싹.수학;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class B5_1001 {
+public class AMinusB_1001 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
