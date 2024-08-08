edaf206c924b2fb7f1f7b8a351bc25a97813068b
gyunny <needle12s@gmail.com>
2024-08-08 10:06:22 +0900

    Bronze Lv5 : 콜센터

---
+++ src/main/java/com/tie/solved/Bronze5/출력/콜센터_5339.java
@@ -0,0 +1,18 @@
+package com.tie.solved.Bronze5.출력;
+
+public class 콜센터_5339 {
+
+    public static void main(String[] args) {
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.print("|          |");
+    }
+
+}
