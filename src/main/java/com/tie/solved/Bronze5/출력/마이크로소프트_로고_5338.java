1952405c9fffb6a97ece8f7f70f7cf719e96063e
gyunny <needle12s@gmail.com>
2024-08-08 09:59:15 +0900

    Bronze Lv5 : 마이크로소프트 로고

--- src/main/java/com/tie/solved/Bronze5/출력/마이크로소프트_로고_5338.java
+++ src/main/java/com/tie/solved/Bronze5/출력/마이크로소프트_로고_5338.java
@@ -1,6 +1,6 @@
 package com.tie.solved.Bronze5.출력;
 
-public class B5_5338 {
+public class 마이크로소프트_로고_5338 {
 
     public static void main(String[] args) {
         System.out.println("       _.-;;-._");
