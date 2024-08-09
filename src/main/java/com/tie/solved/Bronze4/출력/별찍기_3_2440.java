74ea9ec1b73e8667ecd214a9330ab1c85f232677
gyunny <needle12s@gmail.com>
2024-08-09 13:53:43 +0900

    Bronze Lv4 : 별 찍기 -3

--- src/main/java/com/tie/solved/Bronze4/출력/별찍기_3_2440.java
+++ src/main/java/com/tie/solved/Bronze4/출력/별찍기_3_2440.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class B4_2440 {
+public class 별찍기_3_2440 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
