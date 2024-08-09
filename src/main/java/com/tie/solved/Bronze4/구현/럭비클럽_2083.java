7fd05e124b2595f46f4667116fbd916d11ef4645
gyunny <needle12s@gmail.com>
2024-08-09 13:49:59 +0900

    Bronze Lv4 : 럭비 클럽

--- src/main/java/com/tie/solved/Bronze4/구현/럭비클럽_2083.java
+++ src/main/java/com/tie/solved/Bronze4/구현/럭비클럽_2083.java
@@ -1,10 +1,10 @@
-package com.tie.solved.Bronze4.수학;
+package com.tie.solved.Bronze4.구현;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class B4_2083 {
+public class 럭비클럽_2083 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,15 +24,15 @@ public class B4_2083 {
     private static String memberClassification(String s) {
         String[] str = s.split(" ");
 
-        String name = str[0] + " ";
+        String name = str[0];
         int age = Integer.parseInt(str[1]);
         int weight = Integer.parseInt(str[2]);
 
         if (age > 17 || weight >= 80) {
-            return name + "Senior";
+            return name + " Senior";
         }
 
-        return name + "Junior";
+        return name + " Junior";
     }
 
 }
