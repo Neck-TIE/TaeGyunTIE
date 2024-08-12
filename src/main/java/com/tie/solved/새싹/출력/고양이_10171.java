8c7ce9a0527b977bdc5341fa84174b7852fa134a
gyunny <needle12s@gmail.com>
2024-08-12 13:54:25 +0900

    새싹: 고양이

--- src/main/java/com/tie/solved/새싹/출력/고양이_10171.java
+++ src/main/java/com/tie/solved/새싹/출력/고양이_10171.java
@@ -4,7 +4,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
-public class B5_10171 {
+public class 고양이_10171 {
 
     public static void main(String[] args) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
