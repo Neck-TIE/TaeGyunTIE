290b2f829f94b407ccfc6e01bf5b8b5bbff665e9
gyunny <needle12s@gmail.com>
2024-08-12 13:25:26 +0900

    Bronze Lv5 : NFC West vs North

---
+++ src/main/java/com/tie/solved/Bronze5/출력/NFC_WEST_VS_NORTH_10170.java
@@ -0,0 +1,21 @@
+package com.tie.solved.Bronze5.출력;
+
+public class NFC_WEST_VS_NORTH_10170 {
+
+    public static void main(String[] args) {
+        System.out.println("NFC West       W   L  T");
+        System.out.println("-----------------------");
+        System.out.println("Seattle        13  3  0");
+        System.out.println("San Francisco  12  4  0");
+        System.out.println("Arizona        10  6  0");
+        System.out.println("St. Louis      7   9  0");
+        System.out.println();
+        System.out.println("NFC North      W   L  T");
+        System.out.println("-----------------------");
+        System.out.println("Green Bay      8   7  1");
+        System.out.println("Chicago        8   8  0");
+        System.out.println("Detroit        7   9  0");
+        System.out.print("Minnesota      5  10  1");
+    }
+
+}
