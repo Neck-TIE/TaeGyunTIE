9f342d7681fa96b4b340859f8ba850444ea6159d
taegyun1995 <needle12s@gmail.com>
2024-08-14 19:52:44 +0900

    새싹: 사칙연산

--- src/main/java/com/tie/solved/새싹/수학/사칙연산_10869.java
+++ src/main/java/com/tie/solved/새싹/수학/사칙연산_10869.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class B5_10869 {
+public class 사칙연산_10869 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
