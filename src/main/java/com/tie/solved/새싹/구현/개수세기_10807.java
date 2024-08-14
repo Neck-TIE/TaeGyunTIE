6cab12c532b4476243507113231ddce95e3ecb9f
taegyun1995 <needle12s@gmail.com>
2024-08-14 20:25:34 +0900

    새싹: 개수 세기

--- src/main/java/com/tie/solved/새싹/구현/개수세기_10807.java
+++ src/main/java/com/tie/solved/새싹/구현/개수세기_10807.java
@@ -1,11 +1,11 @@
-package com.tie.solved.Bronze5.배열;
+package com.tie.solved.새싹.구현;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class B5_10807 {
+public class 개수세기_10807 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
