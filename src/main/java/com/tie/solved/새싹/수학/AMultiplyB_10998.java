5eca1deec84354499cb2203e19d1ffc98cddba8d
taegyun1995 <needle12s@gmail.com>
2024-08-14 20:10:18 +0900

    새싹: A * B

--- src/main/java/com/tie/solved/새싹/수학/AMultiplyB_10998.java
+++ src/main/java/com/tie/solved/새싹/수학/AMultiplyB_10998.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class B5_10998 {
+public class AMultiplyB_10998 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
