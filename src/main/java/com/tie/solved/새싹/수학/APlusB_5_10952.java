dc38a639b4c7b4be4824eb7ef3c939a95b37cc19
gyunny <needle12s@gmail.com>
2024-08-16 14:10:27 +0900

    새싹 : A + B - 5

---
+++ src/main/java/com/tie/solved/새싹/수학/APlusB_5_10952.java
@@ -0,0 +1,26 @@
+package com.tie.solved.새싹.수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class APlusB_5_10952 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(a == 0 && b ==0) {
+                break;
+            }
+
+            System.out.println(a + b);
+        }
+    }
+
+}
