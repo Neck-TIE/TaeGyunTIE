0aa8c076bf2aed1816500e3b81c93df4a9cee1bb
taegyun1995 <needle12s@gmail.com>
2024-08-18 14:16:55 +0900

    Bronze Lv5 : A + B - 8

---
+++ src/main/java/com/tie/solved/Bronze5/수학/APlusB_8_11022.java
@@ -0,0 +1,29 @@
+package com.tie.solved.Bronze5.수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class APlusB_8_11022 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            String result = solution(i, a, b);
+            System.out.println(result);
+        }
+    }
+
+    private static String solution(int i, int a, int b) {
+        int answer = a + b;
+        return "Case #" + i + ": " + a + " + " + b + " = " + answer;
+    }
+
+}
