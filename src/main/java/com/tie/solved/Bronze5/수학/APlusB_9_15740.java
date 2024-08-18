0945f64b238c5d2c95b4d6fdb2722dbcfd14c373
taegyun1995 <needle12s@gmail.com>
2024-08-18 21:31:00 +0900

    Bronze Lv5 : A + B - 9

--- src/main/java/com/tie/solved/Bronze5/수학/APlusB_9_15740.java
+++ src/main/java/com/tie/solved/Bronze5/수학/APlusB_9_15740.java
@@ -11,7 +11,7 @@ public class APlusB_9_15740 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         BigInteger a = new BigInteger(st.nextToken());
         BigInteger b = new BigInteger(st.nextToken());
 
