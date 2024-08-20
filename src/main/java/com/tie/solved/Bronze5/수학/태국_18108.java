7efbd0fc6810d5a9221c5497701ad0b9653a9781
taegyun1995 <needle12s@gmail.com>
2024-08-20 22:12:17 +0900

    Bronze Lv5 : 1998년생인 내가 태국에서는 2541년생

---
+++ src/main/java/com/tie/solved/Bronze5/수학/태국_18108.java
@@ -0,0 +1,16 @@
+package com.tie.solved.Bronze5.수학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 태국_18108 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int y = Integer.parseInt(br.readLine());
+
+        System.out.println(y - 543);
+    }
+
+}
