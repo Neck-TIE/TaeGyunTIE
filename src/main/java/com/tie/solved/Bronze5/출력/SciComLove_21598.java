d294a63e9d46035ca936d37145580140923fc9d6
taegyun1995 <needle12s@gmail.com>
2024-08-21 23:36:25 +0900

    Bronze Lv5 : SciComLove

---
+++ src/main/java/com/tie/solved/Bronze5/출력/SciComLove_21598.java
@@ -0,0 +1,18 @@
+package com.tie.solved.Bronze5.출력;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SciComLove_21598 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("SciComLove");
+        }
+    }
+
+}
