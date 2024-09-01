8f020e319cf2f0f6c40286f4af4437dec00b2ec2
taegyun1995 <needle12s@gmail.com>
2024-09-01 19:59:37 +0900

    Bronze Lv2 : 단어의 개수

--- src/main/java/com/tie/solved/Bronze2/문자열/단어의개수_1152.java
+++ src/main/java/com/tie/solved/Bronze2/문자열/단어의개수_1152.java
@@ -3,21 +3,15 @@ package com.tie.solved.Bronze2.문자열;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class B2_1152 {
+public class 단어의개수_1152 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String s = br.readLine().trim();
-
-        if(s.equals("")) {
-            System.out.println(0);
-            return;
-        }
-
-        int len = s.split(" ").length;
-        System.out.println(len);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int result = st.countTokens();
+        System.out.println(result);
     }
 
 }
