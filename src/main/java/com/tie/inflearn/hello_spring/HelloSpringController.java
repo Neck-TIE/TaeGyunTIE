312dee9eaba7f8af212c445166cc875454de6f67
taegyun1995 <needle12s@gmail.com>
2024-12-06 16:51:29 +0900

    스프링 셋팅

---
+++ src/main/java/com/tie/inflearn/hello_spring/HelloSpringController.java
@@ -0,0 +1,13 @@
+package com.tie.inflearn.hello_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringController {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringController.class, args);
+    }
+
+}
