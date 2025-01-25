7e88ddf812f7117b472350861f8ddcff3f7b5630
taegyun1995 <needle12s@gmail.com>
2025-01-25 21:30:05 +0900

    문자열 : 단어길이재기

---
+++ src/main/java/com/tie/solved/문자열/브론즈5/단어길이재기_2743.java
@@ -0,0 +1,15 @@
+package com.tie.solved.문자열.브론즈5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 단어길이재기_2743 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(s.length());
+    }
+
+}
