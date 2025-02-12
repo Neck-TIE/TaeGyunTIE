b042b5fa724c9bb6b5637b9aeff3344931eea0c1
taegyun1995 <needle12s@gmail.com>
2025-02-12 12:31:37 +0900

    주석(Comment)

--- src/main/java/com/tie/김영한의자바입문_코드로시작하는자바첫걸음/CommentJava.java
+++ src/main/java/com/tie/김영한의자바입문_코드로시작하는자바첫걸음/CommentJava.java
@@ -1,4 +1,4 @@
-package com.tie.inflearn.java_introduce.comment;
+package com.tie.김영한의자바입문_코드로시작하는자바첫걸음;
 
 public class CommentJava {
 
@@ -10,6 +10,11 @@ public class CommentJava {
         System.out.println("Hello Java3");
         System.out.println("Hello Java4");
         */
+
+        /*
+        주석으로 처리한 코드가 실행되지 않은 것을 확인할 수 있다.
+        즉, 주석은 자바 프로그램이 읽지 않고 무시하는 부분으로, 사람이 읽기 위해서 사용된다.
+         */
     }
 
 }
