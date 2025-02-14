499ad4c3320580cfed13f2b90338b0c7d732c1a9
taegyun1995 <needle12s@gmail.com>
2025-02-14 12:19:17 +0900

    문제와 풀이

---
+++ src/main/java/com/tie/김영한의실전자바_기본편/class1/ex/MovieReview.java
@@ -0,0 +1,6 @@
+package com.tie.김영한의실전자바_기본편.class1.ex;
+
+public class MovieReview {
+    String title;
+    String review;
+}
---
+++ src/main/java/com/tie/김영한의실전자바_기본편/class1/ex/ProductOrder.java
@@ -0,0 +1,7 @@
+package com.tie.김영한의실전자바_기본편.class1.ex;
+
+public class ProductOrder {
+    String productName;
+    int price;
+    int quantity;
+}
