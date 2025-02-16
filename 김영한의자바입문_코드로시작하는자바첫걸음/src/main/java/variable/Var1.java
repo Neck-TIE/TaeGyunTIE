a2e4d255668ee1fdcff22d6875dd80f482df5777
taegyun1995 <needle12s@gmail.com>
2025-02-16 13:56:58 +0900

    변수 시작

--- 김영한의자바입문_코드로시작하는자바첫걸음/src/main/java/variable/Var1.java
+++ 김영한의자바입문_코드로시작하는자바첫걸음/src/main/java/variable/Var1.java
@@ -1,6 +1,8 @@
-package com.tie.inflearn.java_introduce.variable;
+package variable;
 
 public class Var1 {
+
+    // 패키지는 자바 파일을 구분하기 위한 폴더
 
     public static void main(String[] args) {
 /*      System.out.println(10);
@@ -11,5 +13,7 @@ public class Var1 {
         System.out.println(20); // 변경 10 -> 20
         System.out.println(20); // 변경 10 -> 20
     }
+
+    // 변수 : 이름 그대로 변할 수 있다는 뜻
 
 }
