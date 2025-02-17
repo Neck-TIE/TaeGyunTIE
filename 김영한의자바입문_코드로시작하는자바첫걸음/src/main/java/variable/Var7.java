806ec130e663265dcc39a7a30430b4b8df9f449c
taegyun1995 <needle12s@gmail.com>
2025-02-17 23:23:51 +0900

    변수 타입1

--- 김영한의자바입문_코드로시작하는자바첫걸음/src/main/java/variable/Var7.java
+++ 김영한의자바입문_코드로시작하는자바첫걸음/src/main/java/variable/Var7.java
@@ -1,4 +1,4 @@
-package com.tie.inflearn.java_introduce.variable;
+package variable;
 
 public class Var7 {
 
@@ -9,11 +9,17 @@ public class Var7 {
         char d = 'A'; // 문자
         String e = "Hello Java"; // 문자열, 문자열을 다루기 위한 특별한 타입
 
+        // 변수는 데이터를 다루는 종류에 따라 다양한 형식이 존재, 이러한 형식을 타입(type)이라고 함
+
+        // int z = "백원"; // 컴파일 에러 > java: incompatible types: java.lang.String cannot be converted to int
+
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
         System.out.println(d);
         System.out.println(e);
     }
+
+    // 리터럴: 코드에서 개발자가 직접 입력한 고정된 값, 리터럴 자체는 변하지 않음
 
 }
