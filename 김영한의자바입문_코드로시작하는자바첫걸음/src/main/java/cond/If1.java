ab44848f1c1e0890edefd802db5cf24f27126e3a
taegyun1995 <needle12s@gmail.com>
2025-02-18 23:55:12 +0900

    if문1 - if,else

--- 김영한의자바입문_코드로시작하는자바첫걸음/src/main/java/cond/If1.java
+++ 김영한의자바입문_코드로시작하는자바첫걸음/src/main/java/cond/If1.java
@@ -1,16 +1,16 @@
-package com.tie.inflearn.java_introduce.cond;
+package cond;
 
-public class If2 {
+public class If1 {
 
     public static void main(String[] args) {
-        int age = 20;
+        int age = 20; // 사용자 나이
 
         if (age >= 18) {
             System.out.println("성인입니다.");
-        } else {
+        }
+        if (age < 18) {
             System.out.println("미성년자입니다.");
         }
-
     }
 
 }
