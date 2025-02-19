67e9be35e6dffcdf2dade3b423b1a7abf5a55405
taegyun1995 <needle12s@gmail.com>
2025-02-19 23:34:10 +0900

    break, continue

--- 김영한의자바입문_코드로시작하는자바첫걸음/src/main/java/loop/Break1.java
+++ 김영한의자바입문_코드로시작하는자바첫걸음/src/main/java/loop/Break1.java
@@ -1,12 +1,12 @@
-package com.tie.inflearn.java_introduce.loop;
+package loop;
 
-public class Break2 {
+public class Break1 {
 
     public static void main(String[] args) {
         int sum = 0;
         int i = 1;
 
-        for (;;) {
+        while (true) {
             sum += i;
 
             if (sum > 10) {
--- 김영한의자바입문_코드로시작하는자바첫걸음/src/main/java/loop/Break2.java
+++ 김영한의자바입문_코드로시작하는자바첫걸음/src/main/java/loop/Break2.java
@@ -1,12 +1,12 @@
-package com.tie.inflearn.java_introduce.loop;
+package loop;
 
-public class Break1 {
+public class Break2 {
 
     public static void main(String[] args) {
         int sum = 0;
         int i = 1;
 
-        while (true) {
+        for (;;) {
             sum += i;
 
             if (sum > 10) {
